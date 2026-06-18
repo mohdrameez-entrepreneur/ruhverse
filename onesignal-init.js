bfa1679b88dc74f4996bc20a2245a40246d4a3d2
Mohd Rameez <mohdrameez@Mohds-MacBook-Air.local>
2026-06-18 14:42:35 +0530

    onesignal added

--- onesignal-init.js
+++ onesignal-init.js
@@ -4,8 +4,12 @@
   window.OneSignalDeferred.push(async function (OneSignal) {
     await OneSignal.init({
       appId: '31aeb5c9-708a-4537-a231-90f50698b926',
+      safari_web_id: 'web.onesignal.auto.69a0d04c-4cfa-4f80-8d34-652264ce8748',
       serviceWorkerPath: 'push/onesignal/OneSignalSDKWorker.js',
       serviceWorkerParam: { scope: '/push/onesignal/' },
+      notifyButton: {
+        enable: true
+      },
       allowLocalhostAsSecureOrigin: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     });
   });
